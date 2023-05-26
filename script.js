a2266722ae0f3d9dcbc779bb51efbbd845df152a
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-05-26 13:58:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
+let maparr=touristSpots.map(func);
 
+function func(element,index,list) {
+	let temp= element.split(" ");
+	if(temp[0]=='a'|| temp[0]=='an'||temp[0]=='the')
+	{
+		for(let i=1;i<temp.length;i++)
+			{
+				temp[i-1]=temp[i];
+			}
+	}
+	return temp.join(" ");
+}
+
+maparr.sort();
+
+console.log(maparr);
