82dbf00dae0b8ce731d3905eea9f7f1b3238d204
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-05-26 14:58:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 //your code here
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let touristSpots = ["The Virupaksha Temple", "Victoria Memorial", "Tajmahal"];
 
-let maparr=touristSpots.map(func1);
+let maparr = touristSpots.map(func1);
 
 function func1(elem) {
-	let temp=elem.split(" ");
-	let articles=['a','an','the'];
-	let newArr=temp.filter(func2)
-	function func2(word)
-		{
-			return !(articles.inlcudes(word.toLoweCase()));
-		}
-	return newArr.join(" ");
+  let temp = elem.split(" ");
+  let articles = ["a", "an", "the"];
+  let newArr = temp.filter(func2);
+  function func2(word) {
+    let lower = word.toLowerCase();
+    return !articles.includes(lower);
+  }
+  return newArr.join(" ");
 }
 
 maparr.sort();
 
 const ul = document.getElementById("band");
-for(let i=0;i<maparr.length;i++)
-	{
-		let htmlelem=document.createElement("li");
-		htmlelem.innerText=maparr[i];
-		ul.append(htmlelem);
-	}
+for (let i = 0; i < maparr.length; i++) {
+  let htmlelem = document.createElement("li");
+  htmlelem.innerText = maparr[i];
+  ul.append(htmlelem);
+}
 
+console.log(maparr);
